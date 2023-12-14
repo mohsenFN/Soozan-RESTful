b117e299ffbeaec92ec07ba438bcf2cc84ffe65f
none <MohsenFN>
2023-12-14 21:04:36 +0330

    Request model fixed.

--- SoozanProject/Request/models.py
+++ SoozanProject/Request/models.py
@@ -4,8 +4,8 @@ from User.models import User
 class Request(models.Model):
     STATUS_TYPES = (('PENDING', 'Pending'), ('DECLINED', 'Declined'), ('ACCEPTED', 'Accepted'))
         
-    user = models.ForeignKey(User, on_delete = models.CASCADE)
-    # artist = models.ForeignKey(User, on_delete = models.CASCADE) Find a way for this
+    user = models.ForeignKey(User, on_delete = models.CASCADE, related_name = 'fromuser')
+    artist = models.ForeignKey(User, on_delete = models.CASCADE, related_name = 'touser') 
     caption = models.CharField(max_length = 512)
     status = models.CharField(max_length = 8, choices = STATUS_TYPES, default = 'PENDING')
     pub_date = models.DateTimeField()

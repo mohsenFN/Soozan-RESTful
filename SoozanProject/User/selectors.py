f4b6b99f63997eb4ecd7195400ce3d7c2ed17fb2
none <MohsenFN>
2023-12-14 22:50:24 +0330

    User Selector created.

---
+++ SoozanProject/User/selectors.py
@@ -0,0 +1,25 @@
+from User.models import User
+
+class UserSelector:
+
+    def user_is_artist(number):
+        if not User.objects.filter(number=number).exists():
+            raise User.DoesNotExist
+
+        if User.objects.filter(number=number)[0].user_type == 'ARTIST':
+            return True
+
+        return False
+
+
+    def user_is_applicant(number):
+        if not User.objects.filter(number=number).exists():
+            raise User.DoesNotExist
+
+        if User.objects.filter(number=number)[0].user_type == 'USER':
+            return True
+            
+        return False
+        
+    
+    

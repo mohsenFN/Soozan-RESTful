0c7876e7ffec92d2b035fa0f6f57c502b4c48f3d
none <MohsenFN>
2023-12-14 21:10:59 +0330

    User model registered in admin page.

--- SoozanProject/User/admin.py
+++ SoozanProject/User/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from User.models import User
 
-# Register your models here.
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(User, AuthorAdmin)

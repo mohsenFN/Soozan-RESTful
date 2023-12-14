6514d89f30c7628fdeda741a426d780ccd520375
none <MohsenFN>
2023-12-14 21:15:20 +0330

    Applicant model registered in admin page.

--- SoozanProject/Applicant/admin.py
+++ SoozanProject/Applicant/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from Applicant.models import Applicant
 
-# Register your models here.
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Applicant, AuthorAdmin)

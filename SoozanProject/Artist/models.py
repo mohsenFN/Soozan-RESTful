f4d5143cbc1db822c7caf501121f51f8b82cb846
none <MohsenFN>
2023-12-15 23:45:21 +0330

    two new fields added to DerivedArtist model.

--- SoozanProject/Artist/models.py
+++ SoozanProject/Artist/models.py
@@ -8,8 +8,8 @@ class DerivedArtist(models.Model):
     user = models.OneToOneField(User, on_delete = models.CASCADE)
     full_name = models.CharField(max_length = 32)
     art_name = models.CharField(max_length = 32)
+    validity = models.BooleanField(default = False)
     location = models.IntegerField(validators = [MinValueValidator(1), MaxValueValidator(32)]) # BASED ON docs.md
-    posts = None # probably should delete
-    requests = None # probably should delete
+    soozan_score = models.IntegerField(validators = [MinValueValidator(1), MaxValueValidator(5)], default = 1) # MORE INFO docs.md
 
 

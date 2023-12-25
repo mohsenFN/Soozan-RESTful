8c969ee091c4ceff393a2f2ced9ad90fa04d3090
MohsenFn <MaybeEvmo@gmail.com>
2023-12-25 17:53:12 +0330

    removed test prints and cleaner views

--- SoozanProject/User/views.py
+++ SoozanProject/User/views.py
@@ -18,28 +18,23 @@ from Applicant.models import Applicant
 from Applicant.serializers import ApplicantDashBoardSerializer
 
 
-
-
-
-
 @api_view(['GET'])
 def SingleUserView(request: Request, user_id : int):
-
     try:
         queryset = User.objects.get(id = user_id)
     except User.DoesNotExist:
         return Response('user peyda nashod')
 
     serializer = UserSerializer(queryset)
-    
     return Response(serializer.data)
-
 
 
 @api_view(['POST'])
 def Register(request : Request):
 	# Passing user data to serializer
 	user_serializer = UserSerializer(data = request.data, many = False)
+
+	# TODO: check entered number is unique before validating
 
 	if not user_serializer.is_valid():
 		return Response("Invalid Data")
@@ -58,7 +53,7 @@ def Register(request : Request):
 		'''Applicant'''
 		profile = Applicant(user = user)
 
-
+	# saving derived profile model
 	profile.save()
 	
 	# return more logical responses
@@ -70,35 +65,27 @@ def Login(request : Request):
 	number = request.data['number']
 	password = request.data['password']
 	
+	# checking user login data
 	user = authenticate(request,username = number,
 						password = password)
 	
 	if user:
 		login(request, user)
-		print(request.user.is_authenticated)
-		
-		return Response('Welcome')
+		return Response('logged in')
 
 	else:
-		print(request.user.is_authenticated)
 		return Response('Invalid info')
 
 
 @api_view(['GET'])
 def Logout(request : Request):
-
 	logout(request)
 	return Response('Logged out.')
-
 
 
 @login_required()
 @api_view(['GET'])
 def dashboard(request : Request):
-	print(type(request.user), request.user)
-
-	serializer = None
-	queryset = None
 
 	user = User.objects.get(number = request.user)
 
@@ -111,4 +98,3 @@ def dashboard(request : Request):
 		queryset = Applicant.objects.get(user = user)
 		serializer = ApplicantDashBoardSerializer(queryset)
 		return Response(serializer.data)
-

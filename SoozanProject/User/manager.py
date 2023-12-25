7b48cb4fd9c99edd768a3b50e2a9d9be75c773b3
MohsenFn <MaybeEvmo@gmail.com>
2023-12-25 14:02:00 +0330

    create_superuser func added

---
+++ SoozanProject/User/manager.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.base_user import BaseUserManager
+
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, number, password, **kwargs):
+       
+        if not number:
+            raise ValueError("The Phone Number must be set")
+        user = self.model(number=number,password = password, **kwargs)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, number, password, **kwargs):
+        user = self.model(number=number, password = password, **kwargs)
+        user.is_superuser = True
+        user.is_admin = True
+        user.is_staff = True
+        
+        # If did not work set some more boolean fields true LOL
+
+        user.set_password(password)
+        user.save()
+        return user
+

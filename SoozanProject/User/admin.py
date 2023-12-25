5c80b4dfe5c526c635804c28a52b7d5c433ac9a5
MohsenFn <MaybeEvmo@gmail.com>
2023-12-25 14:00:34 +0330

    Custom user registered in admin page.

--- SoozanProject/User/admin.py
+++ SoozanProject/User/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin
+''' Vendor '''
 from User.models import User
 
-class AuthorAdmin(admin.ModelAdmin):
-    pass
+class UserAdminClass(UserAdmin):
+
+    
+    search_fields = ["number", "id"]
+    ordering = ["id"]
+
+    
+    list_filter = []
+    filter_horizontal = []
+    fieldsets = []
 
 
-admin.site.register(User, AuthorAdmin)
+admin.site.register(User, UserAdminClass)
+admin.site.unregister(Group)

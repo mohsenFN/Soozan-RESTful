8fb77ce971375bf468a8ebcfe6422ee8775623f0
MohsenFn <MaybeEvmo@gmail.com>
2023-12-25 17:04:35 +0330

    old viewset removed

--- SoozanProject/Applicant/views.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import viewsets
-from Applicant.serializers import ApplicantSerializer
-from Applicant.models import Applicant
-
-class ApplicantViewSet(viewsets.ModelViewSet):
-    queryset = Applicant.objects.all()
-    serializer_class = ApplicantSerializer

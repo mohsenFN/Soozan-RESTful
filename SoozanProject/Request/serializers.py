ac0b75811c68dbd1b8e868190b5400e56b6d3ca1
MohsenFn <MaybeEvmo@gmail.com>
2023-12-28 00:38:51 +0330

    serilizer added

---
+++ SoozanProject/Request/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from Request.models import Request
+
+
+class NewRequestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Request
+        fields = ['artist', 'caption']
+    
+    def create(self, user, artist, validated_data):
+        req = Request(user = user,
+                        artist = artist,
+                        caption = validated_data['caption'])
+        return req
+
+
+
+
+class ArtistRequestsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Request
+        fields = ['user', 'caption', 'status', 'pub_date']

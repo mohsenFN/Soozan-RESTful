91f1800b0d8c8159e6a52defb9fee02bc0f8f930
MohsenFn <MaybeEvmo@gmail.com>
2024-01-09 12:06:03 +0330

    view for getting tags list

--- SoozanProject/Post/views.py
+++ SoozanProject/Post/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.request import Request
+
+from Post.models import Tag
+from Post.serializers import TagSerializer
+
+
+@api_view(['GET'])
+def get_tags_list(request : Request):
+    queryset = Tag.objects.all()
+
+    serializer = TagSerializer(queryset)
+
+    return Response(serializer.data)

a9632c87353f3ae2738d09199d78cca43519e896
MohsenFn <MaybeEvmo@gmail.com>
2024-01-09 11:39:42 +0330

    Tag model + Image fields for Post model

--- SoozanProject/Post/models.py
+++ SoozanProject/Post/models.py
@@ -1,12 +1,24 @@
 from django.db import models
 from User.models import User
 
+class Tag(models.Model):
+    name_en = models.CharField(max_length=50, unique=True)
+    name_fa = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return f"{self.name_en}/{self.name_fa}"
+
+
 class Post(models.Model):
-    artist = models.ForeignKey(User, on_delete = models.CASCADE)
+    artist = models.ForeignKey(User, on_delete = models.CASCADE, related_name='posts')
     caption =models.CharField(max_length = 512)
-    tags = None # Figure out smt later
+    tags = models.ManyToManyField(Tag)
+    image = models.ImageField(upload_to='post_images/')
     pub_date = models.DateTimeField()
     
+
+    def __str__(self):
+        return f"{self.artist.number}'s Post - {self.caption[:20]}..."
 
 
 

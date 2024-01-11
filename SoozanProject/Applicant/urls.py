905487abd1b6eea94a098a623fd662b62d2424e1
MohsenFn <MaybeEvmo@gmail.com>
2024-01-11 22:14:25 +0330

    removed futile line of codes

--- SoozanProject/Applicant/urls.py
+++ SoozanProject/Applicant/urls.py
@@ -1,10 +1,3 @@
-from django.urls import include, path
-from rest_framework import routers
-from Applicant.views import ApplicantViewSet
+from django.urls import path
 
-router = routers.DefaultRouter()
-router.register('', ApplicantViewSet)
-
-urlpatterns = [
-    path('applicant', include(router.urls))
-]
+urlpatterns = []

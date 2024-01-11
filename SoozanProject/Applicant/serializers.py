b7ba72289fc06480b674182df4c4f7e002a45a50
MohsenFn <MaybeEvmo@gmail.com>
2024-01-11 22:14:51 +0330

    removed futile line of codes

--- SoozanProject/Applicant/serializers.py
+++ SoozanProject/Applicant/serializers.py
@@ -1,8 +1,1 @@
 from rest_framework import serializers
-from Applicant.models import Applicant
-
-
-class ApplicantDashBoardSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Applicant
-        fields = ['user', 'full_name', 'location']

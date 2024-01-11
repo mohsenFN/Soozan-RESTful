a37e2bf974a22e953a742daa8c6300b8f8a3dffa
MohsenFn <MaybeEvmo@gmail.com>
2024-01-11 22:58:19 +0330

    removed dashboard end-point

--- SoozanProject/User/urls.py
+++ SoozanProject/User/urls.py
@@ -4,6 +4,5 @@ from . import views
 urlpatterns = [
     path('register', views.user_register),
     path('login', views.user_login),
-    path('dash', views.get_user_dashboard),
     path('del', views.delete_user)
 ]

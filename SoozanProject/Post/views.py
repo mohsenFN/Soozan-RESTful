51a792ec8702c792e990db9c9818e3ce30a1cc2c
MohsenFn <MaybeEvmo@gmail.com>
2024-01-12 00:24:37 +0330

    view for new posts

--- SoozanProject/Post/views.py
+++ SoozanProject/Post/views.py
@@ -9,7 +9,7 @@ from rest_framework.authentication import TokenAuthentication
 from User.permissions import IsArtistPermission
 
 from Post.models import Tag
-from Post.serializers import TagSerializer
+from Post.serializers import TagSerializer, UploadPostSerializer
 
 
 @api_view(['GET'])
@@ -23,5 +23,12 @@ def get_tags_list(request : Request):
 @api_view(['POST'])
 @authentication_classes([TokenAuthentication])
 @permission_classes([IsArtistPermission])
-def new_post(request : Request):
-    return Response('Hello !')
+def new_post_from_artist(request : Request):
+    serializer = UploadPostSerializer(data = request.data)
+
+    if not serializer.is_valid():
+        # TODO: validate more defined and send explained error msgs
+        return Response({'detail' : 'Invalid data.'})
+    
+    serializer.save()
+    return Response({'detail' : 'data saved'})

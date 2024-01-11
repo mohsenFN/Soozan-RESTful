3501c0dc612af96ca88c54d02d04d76cce871a65
MohsenFn <MaybeEvmo@gmail.com>
2024-01-12 00:23:04 +0330

    new serializer for posts

--- SoozanProject/Post/serializers.py
+++ SoozanProject/Post/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from Post.models import Tag
+from Post.models import Post, Tag
 
 
 class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = '__all__'
+
+
+class UploadPostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
         fields = '__all__'

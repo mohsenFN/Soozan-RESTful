54b6c46adb98f0b20c42c1661f4e0db34a175bae
MohsenFn <MaybeEvmo@gmail.com>
2024-01-12 22:35:06 +0330

    importing drf status

--- SoozanProject/Post/views.py
+++ SoozanProject/Post/views.py
@@ -1,7 +1,7 @@
 from rest_framework.decorators import (
 	api_view, authentication_classes, permission_classes
 )
-
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.request import Request
 from rest_framework.authentication import TokenAuthentication

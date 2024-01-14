9d5da9b5c9248f629026c5d80de9a054590edab4
MohsenFn <MaybeEvmo@gmail.com>
2024-01-14 13:51:04 +0330

    Add Permission for artist authorization

--- SoozanProject/User/permissions.py
+++ SoozanProject/User/permissions.py
@@ -3,3 +3,18 @@ from rest_framework.permissions import BasePermission
 class IsArtistPermission(BasePermission):
     def has_permission(self, request, view):
         return request.user.is_authenticated and request.user.is_artist
+
+
+class IsOwnerOrReadOnly(BasePermission):
+    """
+    Custom permission to only allow owners of an object to edit it.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD, or OPTIONS requests.
+        if request.method in ('GET', 'HEAD', 'OPTIONS'):
+            return True
+
+        # Write permissions are only allowed to the owner of the object.
+        return obj.artist == request.user

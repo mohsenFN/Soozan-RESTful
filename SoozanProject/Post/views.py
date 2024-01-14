084f328c3f6b852609400e3c80e8a13ce2ab34cd
MohsenFn <MaybeEvmo@gmail.com>
2024-01-14 13:28:55 +0330

    better error handling in new_post view

--- SoozanProject/Post/views.py
+++ SoozanProject/Post/views.py
@@ -23,28 +23,11 @@ def get_tags_list(request : Request):
 @api_view(['POST'])
 @authentication_classes([TokenAuthentication])
 @permission_classes([IsArtistPermission])
-def new_post_from_artist(request : Request):
-
-    serializer = UploadPostSerializer(data = request.data)
+def new_post_from_artist(request):
+    serializer = UploadPostSerializer(data=request.data, context={'request': request})
 
     if serializer.is_valid():
-        post = serializer.create(user = request.user,
-                                           validated_data=serializer.validated_data)
-        
-        post.save()
-        return Response({'detail' : 'New post saved', 'post id' : post.id},
-                        status=status.HTTP_201_CREATED)
-    
-    if serializer.data.get('image') in [None, '']:
-        return Response({'detail' : 'No image file is specified'},
-                        status=status.HTTP_400_BAD_REQUEST)
-    
-    if serializer.data.get('caption') in [None, '']:
-        return Response({'detail' : 'No caption is in form-data'},
-                        status=status.HTTP_400_BAD_REQUEST)
-    
-    if serializer.data.get('tags') in [None, '']:
-        return Response({'detail' : 'No tags is in form-data'},
-                        status=status.HTTP_400_BAD_REQUEST)
+        post = serializer.save()
+        return Response({'detail': 'New post saved', 'post_id': post.id}, status=status.HTTP_201_CREATED)
 
-    return Response({'detail' : 'Invalid data.'})
+    return Response({'detail': 'Invalid data', 'errors': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)

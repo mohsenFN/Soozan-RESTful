fc6322db026e71b1e01f978958a7dbce3471fec3
MohsenFn <MaybeEvmo@gmail.com>
2024-01-15 20:42:53 +0330

    Fixed indentation and using consistent qoutation marks

--- SoozanProject/Post/views.py
+++ SoozanProject/Post/views.py
@@ -1,11 +1,13 @@
+# Third-Party Packages
 from rest_framework.decorators import (
-	api_view, authentication_classes, permission_classes
+    api_view, authentication_classes, permission_classes
 )
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.request import Request
 from rest_framework.authentication import TokenAuthentication
 
+# Local Imports
 from User.permissions import IsArtistPermission, IsOwnerOrReadOnly
 
 from Post.models import Post, Tag

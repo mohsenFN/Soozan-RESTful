3de5335d7671fbd569468d9360219ebcf69a52aa
N-1 <maybeevmo@gmail.com>
2024-01-26 06:11:32 -0500

    Add url path for delete_post function view

--- RESTful-API/post/urls.py
+++ RESTful-API/post/urls.py
@@ -4,5 +4,6 @@ from post import views
 urlpatterns = [
     path('tags', views.get_tags_list),
     path('new', views.new_post),
-    path('update/<int:post_id>', views.update_post)
+    path('update/<int:post_id>', views.update_post),
+    path('delete/<int:post_id>', views.delete_post)
 ]

65475e30b20a0d628fde8a8f50ff5550fc8d4166
N-1 <maybeevmo@gmail.com>
2024-02-05 22:46:07 -0500

    🔧 Fix Error : Make jwt middleware compatible with non-authenticated views

--- RESTful-API/user/middleware.py
+++ RESTful-API/user/middleware.py
@@ -1,16 +1,21 @@
 from rest_framework_simplejwt.tokens import RefreshToken
 from .models import BlacklistedToken
+from django.http import HttpResponseForbidden
 
 class BlacklistTokenMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        token = request.META.get('HTTP_AUTHORIZATION', '').split(' ')[1]
+        authorization_header = request.META.get('HTTP_AUTHORIZATION', '')
         
-        if BlacklistedToken.objects.filter(token=token).exists():
-            # Token is blacklisted, handle accordingly (e.g., return 401 Unauthorized)
-            return HttpResponseForbidden("Token is blacklisted")
+        if authorization_header.startswith('Bearer '):
+            # Extract token from the authorization header
+            token = authorization_header.split(' ')[1]
+
+            if BlacklistedToken.objects.filter(token=token).exists():
+                # Token is blacklisted, handle accordingly (e.g., return 401 Unauthorized)
+                return HttpResponseForbidden("Token is blacklisted")
 
         response = self.get_response(request)
         return response

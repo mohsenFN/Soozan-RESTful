3c93f7379f79734d4d6326bbed6d6da57b0ecbbe
Number-1 <maybeevmo@gmail.com>
2024-02-09 19:24:39 +0330

    Create endpoint for logout function view

--- RESTful-API/user/urls.py
+++ RESTful-API/user/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('register', views.user_register),
     path('get-token', views.user_login),
     path('refresh-token', views.new_token),
-    path('delete', views.delete_user)
+    path('delete', views.delete_user),
+    path('logout', views.user_logout)
 ]

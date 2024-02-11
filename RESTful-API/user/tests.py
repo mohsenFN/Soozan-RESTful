5133d9e47a41e40cdf1ebc0b5ab76fc56cc8dd1b
Mohsen <maybeevmo@gmail.com>
2024-02-11 12:00:32 +0330

    Add test for register req with only phone number

--- RESTful-API/user/tests.py
+++ RESTful-API/user/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.urls import reverse
 from rest_framework.test import APIClient
 
+from rest_framework.exceptions import ErrorDetail
 
 class UserRegisterViewTest(TestCase):
     def setUp(self):
@@ -10,4 +11,8 @@ class UserRegisterViewTest(TestCase):
 
     def test_empty_request(self):
         resp = self.client.post(self.url, {})
-        self.assertIn('Invalid Data', resp.data['detail'])
+        self.assertIn('No phone number is specified.', resp.data['detail'])
+
+    def test_request_with_only_phonenumber(self):
+        resp = self.client.post(self.url, {'number' : '09148387871'})
+        self.assertIsInstance(resp.data['errors']['password'][0], ErrorDetail)

946ca3c7059b086fe05978a6c486cc05430f1fee
Mohsen <maybeevmo@gmail.com>
2024-02-11 12:07:12 +0330

    Add test for req with only password in it

--- RESTful-API/user/tests.py
+++ RESTful-API/user/tests.py
@@ -13,6 +13,10 @@ class UserRegisterViewTest(TestCase):
         resp = self.client.post(self.url, {})
         self.assertIn('No phone number is specified.', resp.data['detail'])
 
+    def test_request_with_only_password(self):
+        resp = self.client.post(self.url, {'password' : '11228'})
+        self.assertEqual(resp.data['detail'], 'No phone number is specified.')
+
     def test_request_with_only_phonenumber(self):
         resp = self.client.post(self.url, {'number' : '09148387871'})
         self.assertIsInstance(resp.data['errors']['password'][0], ErrorDetail)

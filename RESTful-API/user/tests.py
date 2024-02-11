e52d7fca718937fa19e1f77ace268c283d990a54
Mohsen <maybeevmo@gmail.com>
2024-02-11 11:43:37 +0330

    Add test for empty data in user register view

--- RESTful-API/user/tests.py
+++ RESTful-API/user/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APIClient
 
-# Create your tests here.
+
+def UserRegisterViewTest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.url = reverse('user-register')
+
+    def empty_request(self):
+        resp = self.client.post(self.url, {})
+        self.assertIn('Invalid Data', resp.data['detail'])

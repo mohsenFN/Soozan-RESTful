91cab4841cb9f0b53032585b307bb285ffad8b08
Mohsen <maybeevmo@gmail.com>
2024-02-13 07:17:48 +0330

    Add used repsones message to REPSONE_MESSGAES dict

---
+++ RESTful-API/utils/user_respones.py
@@ -0,0 +1,13 @@
+
+# RESPONES HERE WILL BE USED IN USER APP
+
+RESPONSE_MESSAGES = {
+    'LOGOUT_OK' : 'Invalidated token and logged out successfully',
+    'DELETE_USER_OK' : 'User deleted successfully.',
+    'INVALID_REFRESH_TOKEN' : 'Invalid refresh token.',
+    'REFRESH_TOKEN_REQ' : 'Refresh token is required.',
+    'AUTH_FAIL' : 'Authentication Failed',
+    'REGISTER_OK' : 'Registered successfuly.',
+    'NO_PHONE_NUMBER' : 'Phone numbers dedicated to an account already.',
+    'DUPLICATE_NUMBER' : 'Phone numbers dedicated to an account already.'
+}

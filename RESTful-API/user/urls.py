11a47c41bae5ea2770a94ce1031c0d82a363f6a5
Mohsen <maybeevmo@gmail.com>
2024-02-21 08:11:20 +0330

    Fix type of view-function name

--- RESTful-API/user/urls.py
+++ RESTful-API/user/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('register', views.user_register, name='user-register'),
-    path('get-token', views.get_tokens, name='get-token'),
+    path('get-token', views.get_token, name='get-token'),
     path('refresh-token', views.refresh_token, name='refresh-token'),
     path('delete', views.delete_user, name='user-delete'),
     path('logout', views.user_logout, name='user-logout')

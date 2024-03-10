21e50a071692813053b171bb3a0eab2762be0273
Mohsen <maybeevmo@gmail.com>
2024-03-10 05:11:06 +0330

    add names for post endpoints

--- RESTful-API/post/urls.py
+++ RESTful-API/post/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from post import views
 
 urlpatterns = [
-    path('tags', views.get_tags_list),
-    path('new', views.new_post),
-    path('update/<int:post_id>', views.update_post),
-    path('delete/<int:post_id>', views.delete_post)
+    path('tags', views.get_tags_list, name='get-tags'),
+    path('new', views.new_post, name='post-new'),
+    path('update/<int:post_id>', views.update_post, name='post-update'),
+    path('delete/<int:post_id>', views.delete_post, name='post-delete')
 ]

fce49fd09494bd04f84cb68b398c549ae8d16063
Mohsen <maybeevmo@gmail.com>
2024-03-10 15:25:52 +0330

    Fix test imgae address

--- RESTful-API/utils/tests.py
+++ RESTful-API/utils/tests.py
@@ -1,6 +1,6 @@
 number = '09148387871'
 password = 'VeryG00dPassword'
-
+test_image_adr = '../11228.jpg'
 def register_and_get_token(client, register_url, login_url):
     client.post(register_url, {'number': number,
                                         'password': password,
@@ -8,3 +8,8 @@ def register_and_get_token(client, register_url, login_url):
     # Get access token
     resp = client.post(login_url, {'number': number, 'password': password})
     return resp.data['access_token']
+
+def test_image():
+    with open(test_image_adr, 'rb') as f:
+            bimage = f.read()
+    return SimpleUploadedFile("test_image.jpg", bimage, content_type="image/jpeg")

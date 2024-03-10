65484b4f9e7ff0283d1f1ac51b0a20ee5f4daefa
Mohsen <maybeevmo@gmail.com>
2024-03-10 07:21:50 +0330

    Add test (app : post) test authed but empty request for new post

--- RESTful-API/post/tests.py
+++ RESTful-API/post/tests.py
@@ -16,8 +16,20 @@ class PostTagsViewTest(TestCase):
 class NewPostViewTest(TestCase):
     def setUp(self):
         self.client = APIClient()
+        self.register_url = reverse('user-register')
+        self.login_url = reverse('get-token')
         self.url = reverse('post-new')
 
     def test_unauth_request(self):
         resp = self.client.post(self.url, {})
         self.assertEqual(401, resp.status_code)
+     
+    def test_authed_empty_request(self):
+        # Register a test user and get access token for posting new post
+        self.client.post(self.register_url, {'number' : '09148387871', 'password' : 'VeryG00dPassword', 'is_artist' : True})
+        resp = self.client.post(self.login_url, {'number' : '09148387871', 'password' : 'VeryG00dPassword'})
+        token = resp.data['access_token']
+        
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {token}')
+        resp = self.client.post(self.url, {})
+        self.assertEqual(400, resp.status_code)

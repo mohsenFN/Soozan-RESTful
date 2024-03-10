674f5b43ed4f106472f62bfb9e7b24841304e833
Mohsen <maybeevmo@gmail.com>
2024-03-10 15:27:18 +0330

    Importing + Adding notes

--- RESTful-API/utils/tests.py
+++ RESTful-API/utils/tests.py
@@ -1,6 +1,10 @@
+from django.core.files.uploadedfile import SimpleUploadedFile
+
+# these data will be used in test functions only
 number = '09148387871'
 password = 'VeryG00dPassword'
 test_image_adr = '../11228.jpg'
+
 def register_and_get_token(client, register_url, login_url):
     client.post(register_url, {'number': number,
                                         'password': password,

b50a8e7d37c3432656a798b489f55b85e2c2732f
Mohsen <maybeevmo@gmail.com>
2024-03-12 06:13:22 +0330

    Use ARTIST_RESPONES in view respones

--- RESTful-API/artist/views.py
+++ RESTful-API/artist/views.py
@@ -5,6 +5,7 @@ from rest_framework.decorators import api_view
 
 from user.models import User
 
+from utils.user_respones import ARTIST_RESPONES
 
 @api_view(['GET'])
 def artist_profile(request: Request, user_id: int):
@@ -13,11 +14,11 @@ def artist_profile(request: Request, user_id: int):
     try:
         user = User.objects.get(id=user_id)
     except User.DoesNotExist:
-        return Response({'detail': 'User does not exist'}, status=status.HTTP_404_NOT_FOUND)
+        return Response({'detail': ARTIST_RESPONES['USER_DOESNOTEXIST']}, status=status.HTTP_404_NOT_FOUND)
 
     # If user is not artist send an error message
     if not user.is_artist:
-        return Response({'detail': 'User does not exist'}, status=status.HTTP_404_NOT_FOUND)
+        return Response({'detail': ARTIST_RESPONES['USER_DOESNOTEXIST']}, status=status.HTTP_404_NOT_FOUND)
 
     # Return artist's profile (or other relevant data)
     return Response({'user_id': user_id})
